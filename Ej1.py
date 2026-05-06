8be04ded60dc87d6bd8df96a47da212bc11e2db4
AlejandroDMaldonado <alejandrodmaldonado2010@gmail.com>
2026-05-06 08:22:12 -0300

    Add files via upload

---
+++ Ej1.py
@@ -0,0 +1,7 @@
+temperatura=int(input("Deci la temperatura"))
+if temperatura<10:
+    print("Hace friooo")
+elif temperatura>25:
+    print("Hace calor")
+elif 11<temperatura<24:
+    print ("Esta templado")
